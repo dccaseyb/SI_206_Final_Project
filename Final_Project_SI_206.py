2fbbbeee33dabf3d900a92ef91ebe65ba6c38d41
Casey Bressler <cbressler@MacBook-Air-472.local>
2022-12-11 18:23:51 -0500

    Code outside of function makes a dictionary of all players drafted from 2010-2015

--- Final_Project_SI_206.py
+++ Final_Project_SI_206.py
@@ -30,16 +30,31 @@ player_dict = {}
 num = 0
 for i in range(len(player_list)):
     data_dict = {}
-    player_info = commonplayerinfo.CommonPlayerInfo(player_id = player_list[i]['id'], headers = custom_headers, timeout = 1000)
+    player_info = commonplayerinfo.CommonPlayerInfo(player_id = player_list[i]['id'], headers = custom_headers, timeout = 100)
     data = player_info.common_player_info.get_json()
     player_data = json.loads(data)
-    if int(player_data["data"][0][24]) >= 2000:
+    if player_data["data"][0][-4] == "Undrafted" or player_data["data"][0][24] == None: # or player_data["data"][0][24] == None, check if this works then remove from line 49
+        pass
+        '''if player_data["data"][0][24] == None:
+            pass
+        elif int(player_data["data"][0][24]) >= 2000:
+            data_dict["First Name"] = player_data["data"][0][1]
+            data_dict["Last Name"] = player_data["data"][0][2]
+            data_dict["College"] = player_data["data"][0][8]
+            data_dict["Draft Year"] = player_data["data"][0][24]
+            data_dict["Draft Round"] = player_data["data"][0][-3]
+            data_dict["Draft Pick (Overall)"] = player_data["data"][0][-2]
+            player_dict[player_data["data"][0][0]] = data_dict
+            print(data_dict)'''
+    #elif player_data["data"][0][24] == None:
+        #pass
+    elif int(player_data["data"][0][-4]) >= 2010 and int(player_data["data"][0][-4]) <= 2015:
         data_dict["First Name"] = player_data["data"][0][1]
         data_dict["Last Name"] = player_data["data"][0][2]
         data_dict["College"] = player_data["data"][0][8]
-        data_dict["Draft Year"] = player_data["data"][0][29]
-        data_dict["Draft Round"] = player_data["data"][0][30]
-        data_dict["Draft Pick"] = player_data["data"][0][31]
+        data_dict["Draft Year"] = player_data["data"][0][-4]
+        data_dict["Draft Round"] = player_data["data"][0][-3]
+        data_dict["Draft Pick (Overall)"] = player_data["data"][0][-2]
         player_dict[player_data["data"][0][0]] = data_dict
         print(data_dict)
     num += 1
@@ -68,14 +83,13 @@ def create_draft_data(id):
     return output_list
 
 # Create Relevant Player list (Drafted 2010-2015)
-def create_yearly_player_list(year):
+def create_player_list():
     output_dict = {}
     player_list = players.get_players()
-    for player in player_list:
-        id = player['id']
+    for player in range(len(player_list)):
+        id = player[i]['id']
         data = create_draft_data(id)
-        if data[3] == year:
-            output_dict[id] = data
+        output_dict[id] = data
     return output_dict
 
 
@@ -92,6 +106,26 @@ def create_draft_data_table(cur, conn):
     cur.execute("CREATE TABLE IF NOT EXISTS Draft_Info (player_id INTEGER PRIMARY KEY, first_name TEXT, last_name TEXT, draft_year INTEGER, draft_round INTEGER, draft_pick INTEGER)") 
     conn.commit()
 
+# Insert Players into Table
+
+# MUST FIND AWAY TO  LIMIT IT TO 25 PLAYERS EACH TIME
+def add_drafted_players(cur, conn):
+    drafted_player_list = create_player_list()
+    for player in drafted_player_list:
+        id = player.index() #find how to get the index for the id
+        f_name = player["First Name"]
+        l_name = player["Last Name"]
+        year = player["Draft Year"]
+        round = player["Draft Pick"]
+        if round == 2:
+            pick = player["Draft Pick (Overall)"] - 30
+        else:
+            pick = player["Draft Pick (Overall)"]
+        cur.execute("INSERT OR IGNORE INTO Draft_Info (player_id, first_name, last_name, draft_year, draft round, draft pick) VALUES (?,?,?,?,?,?)",
+            (id, f_name, l_name, year, round, pick))
+    conn.commit()
+
+
 
 
 
@@ -106,11 +140,12 @@ def main():
 #main()
 
 
-'''id = '76006'
+'''id = '1629071'
 player_info = commonplayerinfo.CommonPlayerInfo(player_id = id, headers = custom_headers, timeout = 100)
 data = player_info.common_player_info.get_json()
 player = json.loads(data)
-print(player)'''
+print(player)
+print((player["data"][0][24]))'''
 
 
 #ayton = drafthistory.DraftHistory(league_id = 00, season_year_nullable = 2018, round_num_nullable = 1, round_pick_nullable = 1,
@@ -125,3 +160,38 @@ def get_draft_pick(year, round, pick):
     r = requests.get(base_url, headers = custom_headers)
     j = json.loads(r.text)
     return j'''
+
+'''player_list = players.get_players()
+print(player_list[24])
+print(len(player_list))
+player_dict = {}
+num = 0
+for i in range(len(player_list)):
+    data_dict = {}
+    player_info = commonplayerinfo.CommonPlayerInfo(player_id = player_list[i]['id'], headers = custom_headers, timeout = 100)
+    data = player_info.common_player_info.get_json()
+    player_data = json.loads(data)
+    print(player_data)
+    player_dict[player_list[i]['id']] = player_data
+    #time.sleep(1)'''
+
+'''player_list = players.get_players()
+print(player_list[24])
+print(len(player_list))
+player_dict = {}
+num = 0
+for i in range(len(player_list)):
+    data_dict = {}
+    player_info = commonplayerinfo.CommonPlayerInfo(player_id = player_list[i]['id'], headers = custom_headers, timeout = 100)
+    data = player_info.common_player_info.get_json()
+    player_data = json.loads(data)
+    print(player_data)
+    time.sleep(1)'''
+
+'''player_list = players.get_players()
+#print(player_list)
+player_info = commonplayerinfo.CommonPlayerInfo(player_id = player_list[24]['id'], headers = custom_headers, timeout = 100)
+data = player_info.common_player_info.get_json()
+player_data = json.loads(data)
+print(player_data)
+'''
